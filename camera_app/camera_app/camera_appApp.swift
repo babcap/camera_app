c8fc15e2cffab8fe2691fbe8de8600f6f266c027
Arthur Batyaev <takerarthur@gmail.com>
2023-07-17 09:07:34 +0200

    Create project

---
+++ camera_app/camera_app/camera_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  camera_appApp.swift
+//  camera_app
+//
+//  Created by Arthur on 17.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct camera_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
