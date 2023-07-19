2ba6659375bf34b0538ca72063ac104dea81c88e
Arthur Batyaev <takerarthur@gmail.com>
2023-07-19 12:38:56 +0200

    Configure camera session

--- camera_app/camera_app/View/ContentView.swift
+++ camera_app/camera_app/View/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-          FrameView(image: nil)
-            .edgesIgnoringSafeArea(.all)
+            FrameView(image: nil)
+                .edgesIgnoringSafeArea(.all)
         }
     }
 }
