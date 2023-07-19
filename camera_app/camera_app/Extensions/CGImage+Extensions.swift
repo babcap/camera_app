cc20412ca9405e5ba631d1762692e267b3adb3f1
Arthur Batyaev <takerarthur@gmail.com>
2023-07-19 12:11:48 +0200

    Convert CVPixelBuffer to CGImage

---
+++ camera_app/camera_app/Extensions/CGImage+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  CGImage+Extensions.swift
+//  camera_app
+//
+//  Created by Arthur on 19.07.2023.
+//
+
+import CoreGraphics
+import VideoToolbox
+
+extension CGImage {
+  static func create(from cvPixelBuffer: CVPixelBuffer?) -> CGImage? {
+    guard let pixelBuffer = cvPixelBuffer else {
+      return nil
+    }
+
+    var image: CGImage?
+    VTCreateCGImageFromCVPixelBuffer(
+      pixelBuffer,
+      options: nil,
+      imageOut: &image)
+    return image
+  }
+}
